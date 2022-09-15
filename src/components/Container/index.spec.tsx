0631a18aeee1b2cde06f541ef9897c33bc38a3db
hleao <henrique.leao@softvaro.com.br>
2022-09-15 08:01:45 -0300

    feat: :sparcles: container component

---
+++ src/components/Container/index.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Container } from '.';
+import { renderWithTheme } from 'utils/tests/renderWithTheme';
+import theme from 'styles/theme';
+
+const makeSut = () => {
+  const sut = renderWithTheme(<Container />);
+
+  return {
+    sut
+  };
+};
+
+describe('Container', () => {
+  it('should be render a Container', () => {
+    const { sut } = makeSut();
+
+    expect(sut.container.firstChild).toHaveStyle({
+      'max-width': theme.grid.container
+    });
+
+    expect(sut.container).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
+});
